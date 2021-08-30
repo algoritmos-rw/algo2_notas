4f27cd72d4a9fb3d7accdcd9acc98027991733a7
tutecano1995 <tutecano1995@gmail.com>
2021-08-30 15:01:38 -0300

    Usar service account para acceder a la planilla (#7)

--- notas_oauth.py
+++ notas_oauth.py
@@ -5,30 +5,50 @@
 import datetime
 import httplib2
 import os
-import sys
 
 import oauth2client.client
+import google.oauth2.service_account
+import google.auth.transport.requests
 
 _CLIENT_ID = os.environ["NOTAS_OAUTH_CLIENT"]
 _CLIENT_SECRET = os.environ["NOTAS_OAUTH_SECRET"]
 _OAUTH_REFRESH = os.environ["NOTAS_REFRESH_TOKEN"]
+_SERVICE_ACCOUNT_JSON = os.environ["NOTAS_SERVICE_ACCOUNT_JSON"]
 
-_creds = oauth2client.client.OAuth2Credentials(
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/gmail.send"]
+
+# TODO: Unificar autenticacion para planilla y cuenta de mail.
+# Por ahora no encontramos la forma de enviar mails usando la service account.
+# Mantenemos por un lado el service account para acceder a la planilla y el client id/secret para enviar mails.
+_creds_spreadhseet = google.oauth2.service_account.Credentials.from_service_account_file(_SERVICE_ACCOUNT_JSON, scopes=SCOPES)
+_creds_email = oauth2client.client.OAuth2Credentials(
     "", _CLIENT_ID, _CLIENT_SECRET, _OAUTH_REFRESH,
     datetime.datetime(2015, 1, 1),
     "https://accounts.google.com/o/oauth2/token", "notasweb/1.0")
 
 
-def get_credenciales():
-    """Devuelve nuestro objeto OAuth2Credentials, actualizado.
+def get_credenciales(creds, es_valida, refrescar):
+    if not es_valida(creds):
+        refrescar(creds)
+    return creds
 
+
+def get_credenciales_spreadsheet():
+    """Devuelve nuestro objeto OAuth2Credentials para acceder a la planilla, actualizado.
     Esta función llama a _refresh() si el token expira en menos de 5 minutos.
     """
-    now = datetime.datetime.utcnow()
-    valid_until = _creds.token_expiry - datetime.timedelta(minutes=5)
+    return get_credenciales(
+        _creds_spreadhseet,
+        lambda creds: creds.valid,
+        lambda creds: creds.refresh(google.auth.transport.requests.Request())
+    )
 
-    if valid_until < now:
-        print("Generando nuevo token de acceso.", file=sys.stderr)
-        _creds.refresh(httplib2.Http())
-
-    return _creds
+def get_credenciales_email():
+    """Devuelve nuestro objeto OAuth2Credentials para acceder al mail, actualizado.
+    Esta función llama a _refresh() si el token expira en menos de 5 minutos.
+    """
+    return get_credenciales(
+        _creds_email,
+        lambda creds: creds.token_expiry - datetime.timedelta(minutes=5) > datetime.datetime.utcnow(),
+        lambda creds: creds.refresh(httplib2.Http())
+    )
